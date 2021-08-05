5e815a27a0a5a84b49482e18c1d32d418b6443fc
Nangnaa <86641797+Nangnaa@users.noreply.github.com>
2021-08-05 12:31:35 +0900

    210803

---
+++ src/network/TcpIpMultichatClient.java
@@ -0,0 +1,76 @@
+package network;
+
+import javax.swing.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class TcpIpMultichatClient {
+  public static void main(String[] args) {
+    String nickName = JOptionPane.showInputDialog("채팅할 대화명을 입력하세요!");
+    String ip = JOptionPane.showInputDialog("서버 IP를 입력하세요!");;
+    try {
+      Socket socket = new Socket(ip, 7777);
+      System.out.println("서버에 접속되었습니다.");
+      Sender sender = new Sender(socket, nickName);
+      sender.start();
+      Receiver receiver = new Receiver(socket);
+      receiver.start();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  static class Sender extends Thread {
+    Scanner scan = new Scanner(System.in);//키보드입력받기 위한 인스턴스
+    Socket socket; //네트워크를 사용하기 위한 통로
+    DataOutputStream out;//입출력스트림(socket에서 획득 그래야 그 네트워크로 보낼수 있음
+    String nickName;//통신할 때 구분되는 ID같은 개념
+
+    //위의 변수들을 생성자를 통해서 초기화!
+    public Sender(Socket socket, String nickName) {
+      this.socket = socket; this.nickName = nickName;
+      try {
+        //소켓으로부터 출력스트림을 획득
+        out = new DataOutputStream(socket.getOutputStream());
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+
+    @Override
+    public void run() {
+      try {
+        if(out!=null) out.writeUTF(nickName);
+        while (out != null) {
+          out.writeUTF("["+nickName+"]"+scan.nextLine());
+        }
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  static class Receiver extends Thread {
+    Socket socket;
+    DataInputStream in;
+    Receiver(Socket socket){
+      this.socket = socket;
+      try {in = new DataInputStream(socket.getInputStream());}catch(IOException e){}
+    }
+
+    @Override
+    public void run() {
+      while (in != null) {
+        try {
+          System.out.println(in.readUTF());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+  }
+
+}
